e644bc56d4b1b00c7cd7d94a7ddef1244c7f5e6b
AbdiN7 <abdirahman.noor@smoothstack.com>
2019-12-04 15:02:07 -0500

    docker works now

--- server.js
+++ server.js
@@ -6,6 +6,12 @@ const app = express();
 const cors = require('cors');
 const CONFIG = require('./config');
 
+// app.use(function(req, res, next) {
+//     res.header("Access-Control-Allow-Origin", "*");
+//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//     next();
+// });
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false}));
 app.use(cors());
@@ -14,7 +20,7 @@ app.get('/', function (req, res) {
     res.send('Server is up!');
 });
 const Users = require('./src/routes/Users');
-app.use('/users', Users)
+app.use('/users', Users);
 
 app.listen(`${CONFIG.port}`, function () {
     console.log(`app listening on port ${CONFIG.port} `);
